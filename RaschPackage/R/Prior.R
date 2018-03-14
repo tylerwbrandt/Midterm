c790f48284612f4c9a7a59e8c8b566db598671b7
Tyler Brandt <tylerbrandt02@gmail.com>
2018-03-14 16:17:26 -0400

    Prior Documentation
    Creating Prior function .R file. Wrote roxygen code for the documentation file.

---
+++ RaschPackage/R/Prior.R
@@ -0,0 +1,16 @@
+#' Finding prior for a given value of theta
+#'
+#' Finds height of normal curve with mean = 0, sd = 3 at theta
+#'
+#' @param theta A numeric object.
+#'
+#' @return An object of class Numeric that represents a prior for theta
+#' @author Tyler Brandt
+#' @note This function is for test takers
+#' @examples
+#' 
+#' myTheta <- 3
+#' Prior(myTheta)
+#' @rdname Prior
+#' @aliases Prior, Prior Numeric Method
+#' @export
