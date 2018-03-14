911e57b72029400c9a589518051d89989d528e72
Tyler Brandt <tylerbrandt02@gmail.com>
2018-03-14 15:24:40 -0400

    Created Rasch student
    Created Rasch student to use when creating functions.

--- DevelopmentFile.R
+++ DevelopmentFile.R
@@ -2,7 +2,7 @@ library(devtools)
 library(roxygen2)
 
 
-new(Class = "Rasch", name = "Tyler", a = c(1,2,3), y_j = c(1,2,3))
+tyler <- new(Class = "Rasch", name = "Tyler", a = c(1,2,3), y_j = c(1,2,3))
 
 setwd("~/Documents/Applied_Statistical_Programming/Midterm")
 package.skeleton()
