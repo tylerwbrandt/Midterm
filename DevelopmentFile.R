be312385fa10a0c49130b68f28bb966cc32b7018
Tyler Brandt <tylerbrandt02@gmail.com>
2018-03-14 15:19:19 -0400

    Package Skeleton
    Created Package Skeleton

--- DevelopmentFile.R
+++ DevelopmentFile.R
@@ -14,3 +14,6 @@ setClass(Class = "Rasch",
          ))
 
 new(Class = "Rasch", name = "Tyler", a = c(1,2,3), y_j = c(1,2,3))
+
+setwd("~/Documents/Applied_Statistical_Programming/Midterm")
+package.skeleton()
