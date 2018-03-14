aeec38a29746e279db1f8dadb8070cf8888aa786
Tyler Brandt <tylerbrandt02@gmail.com>
2018-03-14 15:20:50 -0400

    Rasch Class
    Created Documentation for Rasch class.

---
+++ RaschPackage/R/Rasch.R
@@ -0,0 +1,16 @@
+#' A student who took a test with his difficulty and answers
+#' 
+#' Object of class \code{Rasch} are used in the \code{Probability}, \code{Likelihood}, \code{Prior}, \code{EAP} functions
+#'
+#' 
+#' An object of the class 'Rasch' has the following slots:
+#' \itemize{
+#' \item \code{name} The name of the student who took the test
+#' \item \code{a} The difficulties of the questions on the test
+#' \item \code{y_j} The student's answers
+#' }
+#'
+#' @author Tyler Brandt" \email{brandttyler@@wustl.edu}
+#' @aliases Rasch Initialize
+#' @rdname Rasch
+#' @export
